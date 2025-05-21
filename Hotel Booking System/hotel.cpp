11c25b8044eee175e6bc72abb3a99685d2030f09
Mark Rafat <120990809+mark11257@users.noreply.github.com>
2025-05-21 18:11:21 +0300

    finished booking system

--- Hotel Booking System/hotel.cpp
+++ Hotel Booking System/hotel.cpp
@@ -14,9 +14,13 @@ public:
         cout<<"Room "<<room.getRoomNumber()<<" Added succesfully\n";
     }
     void displayRooms() {
+        bool flag=false;
         for(Room& room : rooms){
             room.displayRoom();
+            flag=true;
         }
+        if(!flag)
+            cout<<"there are no rooms in the hotel\n";
     }
 
     void searchRoom(int number){
@@ -45,7 +49,19 @@ public:
             cout<<"Could not book the room, Either room is already booked or not found\n";
         return bookedRoom;
     }
-   
-    
-    
+
+   void showAvailableRooms(){
+    int countAvailableRooms = 0;
+    for(Room& room:rooms){
+        if(!room.isBooked()){
+            countAvailableRooms++;
+            cout<<"Room Number "<<room.getRoomNumber()<<" is Available\n";
+        }  
+    }
+     cout<<countAvailableRooms<<" Rooms are Available to Book.\n";
+   }
+
+
+
+
 };
